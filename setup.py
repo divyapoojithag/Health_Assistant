4776dc9f7cc8f3d43609b60cb25fe655abafebec
divyapoojithag <divya.poojitha@analistadata.ai>
2025-02-27 13:49:16 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Health_Assistant_pkg',
+    version = '0.0.0',
+    author = 'divyapoojithag',
+    author_email = 'divya.poojitha@analistadata.ai',
+    packages= find_packages(),
+    install_requires = []
+)
